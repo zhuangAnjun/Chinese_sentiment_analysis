bef64bd014a549acd08cad554397143c722f7d6c
zhuangAnjun <547755010@qq.com>
2018-06-08 21:37:09 +0800

    规范了代码，增加了文件说明

--- sampling.py
+++ sampling.py
@@ -1,5 +1,6 @@
 import numpy as np
 from random import randint
+from keras.utils import to_categorical
 
 # 对数据进行欠采样和过采样，使数据平衡。 注意输入为原始读取文件获得的数据
 def sampling(train_x, train_y, numOfdatas=25000, isSampling=True):
@@ -22,11 +23,11 @@ def sampling(train_x, train_y, numOfdatas=25000, isSampling=True):
         
     # 不进行采样
     else:
-    X_train = np.array(train_x[0:82000][0])
-    X_train = X_train.tolist()
-    y_train = keras.utils.to_categorical(train_y, num_classes=3)
-    y_train = np.array(y_train[0:82000][:])
-    y_train = y_train.tolist()
+        X_train = np.array(train_x[0:82000][0])
+        X_train = X_train.tolist()
+        y_train = to_categorical(train_y, num_classes=3)
+        y_train = np.array(y_train[0:82000][:])
+        y_train = y_train.tolist()
     return X_train, y_train
 
 
